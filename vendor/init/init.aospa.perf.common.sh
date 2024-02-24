d9edda3799d7150148e7b7341f192ca1b5b2d389
EmanuelCN <emanuelghub@gmail.com>
2024-02-24 09:28:03 +0000

    vendor: init: move KGSL and MSM_DRM to cores 6 and 7
    By putting it on the bigger cores they can be processed quicker resulting in improved responsiveness
    
    *Also no effect on battery life from my testing on SM8550
    
    Change-Id: I13e710c94ff80cb5a044547989bda9a56d94b9e7

--- vendor/init/init.aospa.perf.common.sh
+++ vendor/init/init.aospa.perf.common.sh
@@ -13,10 +13,10 @@ function write_irq_affinity() {
 }
 
 # IRQ Tuning
-# kgsl_3d0_irq -> CPU 1
-# msm_drm -> CPU 2
-write_irq_affinity kgsl_3d0_irq 1
-write_irq_affinity msm_drm 2
+# kgsl_3d0_irq -> CPU 6
+# msm_drm -> CPU 7
+write_irq_affinity kgsl_3d0_irq 6
+write_irq_affinity msm_drm 7
 
 # IRQ Tuning for 5.15 and newer targets
 write_irq_affinity hfi 1
